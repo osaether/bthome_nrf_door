33f08a7764619b621131e94713016feff0409018
Ole Sæther <osaether@gmail.com>
2023-02-26 19:36:56 +0100

    Added initial files.
    Signed-off-by: Ole Sæther <osaether@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # bthome_nrf_door
 BTHome door sensor using the nRF52
+
+This project implements a [BTHome](https://bthome.io/) door sensor with nRF52.
+
+It assumes that a digital signal is connected to GPIO0.2 that is 1 when the door is
+closed and 0 when the door is open.
+
+# Building
+
+You need the [nRF Connect SDK](https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/nrf/index.html) to build this sample.
+
+Clone this repo in a folder under the samples folder in the nRF Connect SDK.
+To build for the nRF52840 DK run the west build command:
+
+```shell
+west build -b nrf52840dk_nrf52840
+```
+
+To build for other devices/boards replace nrf52840dk_nrf52840 with the correct one.
---
+++ src/main.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/drivers/gpio.h>
+
+#define SERVICE_DATA_LEN        5
+#define SERVICE_UUID            0xfcd2		// BTHome service UUID
+
+#define DOOR_PIN                2
+
+static const struct device *gpio_port0_dev = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+
+
+#define ADV_PARAM BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_IDENTITY, \
+					     BT_GAP_ADV_SLOW_INT_MIN, \
+					     BT_GAP_ADV_SLOW_INT_MAX, NULL)
+
+static uint8_t service_data[SERVICE_DATA_LEN] = { 
+	BT_UUID_16_ENCODE(SERVICE_UUID),
+	0x40,
+	0x1A,	// Door
+	0x00
+};
+
+static struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+	BT_DATA(BT_DATA_SVC_DATA16, service_data, ARRAY_SIZE(service_data))
+};
+
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	/* Start advertising */
+	err = bt_le_adv_start(ADV_PARAM, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+}
+
+void init(void)
+{
+	int err;
+
+    gpio_pin_configure(gpio_port0_dev, DOOR_PIN, GPIO_INPUT | GPIO_PULL_DOWN);
+	
+	/* Initialize the Bluetooth Subsystem */
+	err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+	}
+}
+
+void main(void)
+{
+	int err;
+
+	printk("Starting BTHome door sensor\n");
+
+	init();
+
+	for (;;) {
+		int door_state = gpio_pin_get(gpio_port0_dev, DOOR_PIN);
+		service_data[4] = (door_state ^ 1) & 0xff;
+		err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), NULL, 0);
+		if (err) {
+			printk("Failed to update advertising data (err %d)\n", err);
+		}
+		k_sleep(K_MSEC(BT_GAP_ADV_FAST_INT_MAX_2));
+	}
+}
